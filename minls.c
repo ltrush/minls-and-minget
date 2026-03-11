610d2fca78470aa98bad4e388105489bc8cdc32e
jaxonsprogis <jaxonsprogis@gmail.com>
2026-03-10 17:56:40 -0700

    need to add read files stuff

--- minls.c
+++ minls.c
@@ -7,6 +7,7 @@
 /**
  * QUESTIONS:
  * does the order of args matter? for some reason -v using his minls doesnt work
+ * should base be a uint32 or uint64
 */
 
 /**
@@ -22,6 +23,12 @@
 #define PART_TAB_SIG_2          0xAA
 #define SIG_1_OFFSET            510
 #define SIG_2_OFFSET            511
+#define MINIX_MAGIC_NUM         0x4D5A
+#define SUPERBLOCK_OFFSET       1024
+#define I_BLOCK_OFFSET          2
+#define DIRECT_ZONES            7
+#define INODE_SIZE              64
+#define ROOT_INODE              1
 
 #define MAX_PARTITION_NUM       3
 #define NO_PARTITION            -1
@@ -39,23 +46,35 @@ struct __attribute__((packed)) partition_entry {
     uint32_t size;
 };
 
-struct __attribute((packed)) superblock {
+struct __attribute__((packed)) superblock {
     uint32_t ninodes; /* number of inodes in this filesystem */
     uint16_t pad1; /* make things line up properly */
     int16_t i_blocks; /* # of blocks used by inode bit map */
-    int16 t z blocks; /* # of blocks used by zone bit map */
-    uint16 t firstdata; /* number of first data zone */
-int16 t log zone size; /* log2 of blocks per zone */
-int16 t pad2; /* make things line up again */
-uint32 t max file; /* maximum file size */
-uint32 t zones; /* number of zones on disk */
-int16 t magic; /* magic number */
-int16 t pad3; /* make things line up again */
-uint16 t blocksize; /* block size in bytes */
-uint8 t subversion; /* filesystem sub–version */
-}
+    int16_t z_blocks; /* # of blocks used by zone bit map */
+    uint16_t firstdata; /* number of first data zone */
+    int16_t log_zone_size; /* log2 of blocks per zone */
+    int16_t pad2; /* make things line up again */
+    uint32_t max_file; /* maximum file size */
+    uint32_t zones; /* number of zones on disk */
+    int16_t magic; /* magic number */
+    int16_t pad3; /* make things line up again */
+    uint16_t blocksize; /* block size in bytes */
+    uint8_t subversion; /* filesystem sub–version */
+};
 
-
+struct __attribute__((packed)) inode {
+    uint16_t mode; /* mode */
+    uint16_t links; /* number or links */
+    uint16_t uid;
+    uint16_t gid;
+    uint32_t size;
+    int32_t atime;
+    int32_t mtime;
+    int32_t ctime;
+    uint32_t zone[DIRECT_ZONES];
+    uint32_t indirect;
+    uint32_t two_indirect;
+    uint32_t unused;
 }
 
 struct options {
@@ -67,10 +86,16 @@ struct options {
 };
 
 void get_options(int argc, char *argv[], struct options *my_options);
-uint32_t get_partition_lfirst(FILE *disk, uint32_t sector, int8_t partition_num);
-unsigned long find_base(FILE *disk, int8_t partition, int8_t subpartition);
+uint32_t get_partition_lfirst(uint32_t sector, int8_t partition_num);
+uint32_t find_base(int8_t partition, int8_t subpartition);
 
-uint32_t get_partition_lfirst(FILE *disk, uint32_t sector, int8_t partition_num) {
+FILE *disk;
+uint32_t base;
+uint32_t zone_size;
+/*how far inode table is away from base*/
+uint32_t inode_table_offset;
+
+uint32_t get_partition_lfirst(uint32_t sector, int8_t partition_num) {
     uint8_t sigs[2];
     uint32_t base_in_bytes = (sector * SECTOR_SIZE);
     fseek(disk, base_in_bytes + SIG_1_OFFSET, SEEK_SET);
@@ -94,18 +119,18 @@ uint32_t get_partition_lfirst(FILE *disk, uint32_t sector, int8_t partition_num)
     return my_partition_entry.lFirst;
 }
 
-unsigned long find_base(FILE *disk, int8_t partition, int8_t subpartition) {
+uint32_t find_base(int8_t partition, int8_t subpartition) {
     if (partition == NO_PARTITION) {
         return 0;
     }
 
-    uint32_t first_sector = get_partition_lfirst(disk, 0, partition);
+    uint32_t first_sector = get_partition_lfirst(0, partition);
 
     if (subpartition == NO_PARTITION) {
         return first_sector * SECTOR_SIZE;
     }
 
-    first_sector = get_partition_lfirst(disk, first_sector, subpartition);
+    first_sector = get_partition_lfirst(first_sector, subpartition);
     return first_sector * SECTOR_SIZE;
 
 // /* then access the partition you want by index */
@@ -172,21 +197,63 @@ void get_options(int argc, char *argv[], struct options *my_options) {
                             my_options->path);
 }
 
+void read_inode_n(uint32_t inode_num, struct inode * my_inode) {
+    /*inode are not 0 indexed and start at 1*/
+    uint32_t inode_addr = base + inode_table_offset + (inode_num - 1) * INODE_SIZE;
+    fseek(disk, inode_addr, SEEK_SET);
+    fread(my_inode, sizeof(struct inode), 1, disk);
+}
+
+void get_superblock(struct superblock * sb) {
+    uint32_t sb_start = base + SUPERBLOCK_OFFSET;
+    fseek(disk, sb_start, SEEK_SET);
+    fread(sb, sizeof(struct superblock), 1, disk);
+
+    if (sb->magic != MINIX_MAGIC_NUM) {
+        fprintf(stderr, "This doesn't look like a MINIX filesystem.\n");
+        exit(EXIT_FAILURE);
+    }
+    zone_size = sb->blocksize << sb->log_zone_size;
+    uint32_t inode_table_block = I_BLOCK_OFFSET + sb->i_blocks + sb->z_blocks;
+    inode_table_offset = base + (inode_table_block * sb->blocksize);
+}
+
+void read_file(char * buffer, uint32_t file_inode_num) {
+    struct inode file_inode;
+    read_inode_n(file_inode_num, &file_inode);
+    int bytes_remaining = file_inode.size;
+    int zone_index = 0;
+
+    while (bytes_remaining > 0 ) {
+        
+    }
+}
+
+uint32_t name_to_inode(uint32_t directory_inode_num, char * name, int name_len) {
+    
+
+
+
+}
+
 int main(int argc, char *argv[]) {
     struct options my_options = {0};
     get_options(argc, argv, &my_options);
+    struct superblock sb;
+    struct inode root;
 
-    FILE *fp = fopen(my_options.imagefile, "rb");
-    if (fp == NULL) {
+    disk = fopen(my_options.imagefile, "rb");
+    if (disk == NULL) {
         perror("fopen");
         return EXIT_FAILURE;
     }
-    unsigned long base = find_base(fp, 
-                                my_options.partition, 
-                                my_options.subpartition);
+    base = find_base(my_options.partition, my_options.subpartition);
 
-    get_superblock()
-    unsigned long superblock_offset = base + 1024;
+    get_superblock(&sb);
+    get_destination(path,)
+        while(pa)
+    get_inode_n(1,&my_inode);
+    
 
 
     if (base == 0) {
